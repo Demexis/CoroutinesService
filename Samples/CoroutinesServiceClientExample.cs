eb7e83f9d992dd7577f85786e14e46d57f9665d6
Demexis <53381204+Demexis@users.noreply.github.com>
2024-06-26 17:59:29 +0300

    Update CoroutinesServiceClientExample.cs

--- Samples/CoroutinesServiceClientExample.cs
+++ Samples/CoroutinesServiceClientExample.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using JetBrains.Annotations;
 using UnityEngine;
 
@@ -6,10 +6,12 @@ namespace CustomCoroutines.Example {
     public class CoroutinesServiceClientExample : MonoBehaviour {
         [SerializeField] private CoroutinesServiceInstallerExample coroutinesServiceInstaller;
         
-        private const int COUNTER_MAX_VALUE = 5;
+        private const int COUNTER_MAX_VALUE = 3;
 
         private ICoroutinesService coroutinesService;
         private CoroutinePlaybackInfo? pressedButtonCoroutine;
+
+        private int buttonClicksCounter;
         
         private void Start() {
             coroutinesService = coroutinesServiceInstaller.CoroutinesService;
@@ -20,18 +22,35 @@ namespace CustomCoroutines.Example {
             // stop previous coroutine if active
             coroutinesService.StopCoroutine(pressedButtonCoroutine);
             
+            // increase clicks counter by 1
+            buttonClicksCounter++;
+            Debug.Log($"<color=yellow>Button pressed.</color> Total clicks: <color=yellow>{buttonClicksCounter}</color>");
+            
             // start new coroutine playback
             pressedButtonCoroutine = coroutinesService.PlayCoroutine("TEST_COROUTINE_NAME", () => TestEnumeratorMethod(true),
-                () => this);
+                () => FailOnThirdClick(), new CoroutinePlaybackData() {
+                    onBreakCallback = () => Debug.Log("<color=#FF0000>Check-callback failed, that caused playback to stop.</color>"),
+                    onFinishedCallback = () => Debug.Log("<color=#00FF00>Coroutine playback successfully finished.</color>"),
+                    onEndCallback = () => Debug.Log("<color=#00BAFF>Coroutine playback ended.</color>")
+                });
         }
-
+        
         private IEnumerator TestEnumeratorMethod(bool useDebugLogs) {
             for (var i = 0; i < COUNTER_MAX_VALUE; i++) {
                 if (useDebugLogs) {
-                    Debug.Log("Ticking: " + i);
+                    Debug.Log($"Ticking: <color=yellow>{i}</color>");
                 }
                 yield return new WaitForSeconds(1f);
             }
-        } 
+        }
+
+        private bool FailOnThirdClick() {
+            if (!this) {
+                // GameObject / Component has been destroyed. Coroutine needs to be manually stopped. Just return 'false'.
+                return false;
+            }
+            
+            return buttonClicksCounter % 3 != 0;
+        }
     }
 }
